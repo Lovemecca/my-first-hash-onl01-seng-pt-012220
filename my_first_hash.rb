7480f7874de1f508f42e400183e7c0fc8c586fba
Mecca Jay <mecca.nicolej@gmail.com>
2020-02-03 15:52:15 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,10 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+my_hash = {
+    "my_name" => "Mecca",
+    "my_height" => "5 feet 6 inches",
+    "my_weight" => "145 lbs",
+  }
 end
 
 
